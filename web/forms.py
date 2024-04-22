e0fafd5375be75f49e5aeac7341d521d9545f8f9
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-04-22 13:12:30 -0400

    se quitan labels

--- web/forms.py
+++ web/forms.py
@@ -5,11 +5,11 @@ class ContactFormForm(forms.ModelForm):
     class Meta:
         model = ContactForm
         fields = ['customer_email','customer_name','message']
-        labels = {
-            'customer_email': 'Correo Electrónico',
-            'customer_name': 'Nombre',
-            'message': 'Mensaje'
-            }
+        #labels = {
+         #   'customer_email': 'Correo Electrónico',
+          #  'customer_name': 'Nombre',
+           # 'message': 'Mensaje'
+            #}
 #class ContactForm(forms.Form):
     #customer_email = forms.EmailField(label='Email')
     #customer_name = forms.CharField(label='Name', max_length=64)
